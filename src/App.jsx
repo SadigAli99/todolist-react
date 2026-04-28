1bd35c1bc076fb5fe1953962a152a9f121bca09d
SadigAli <sadig.aliev99@gmail.com>
2026-04-28 14:45:58 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import ToDoListItem from "./ToDoListItem";
+import { PlusIcon } from "@heroicons/react/16/solid";
 
 const App = () => {
-  return (
-    <div className="dark:bg-gray-800 rounded-lg px-6 py-8 w-3/5 mx-auto">
-      <h2 className="text-center text-3xl font-bold">To Do List</h2>
-      <div className="to-do-list mt-[20px]">
-        <ToDoListItem title="Item 1" />
-        <ToDoListItem title="Item 2" />
-        <ToDoListItem title="Item 3" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="dark:bg-gray-800 rounded-lg px-6 py-8 w-3/5 mx-auto">
+            <h2 className="text-center text-3xl font-bold">To Do List</h2>
+
+            <div className="flex justify-end my-[10px]">
+                <button className="cursor-pointer outline outline-offset-4 outline-blue-500 rounded-[2px]">
+                    <PlusIcon className="w-5 h-5 text-blue-500" />
+                </button>
+            </div>
+
+            <div className="to-do-list mt-[20px]">
+                <ToDoListItem title="Item 1" />
+                <ToDoListItem title="Item 2" />
+                <ToDoListItem title="Item 3" />
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/ToDoListItem.jsx
+++ src/ToDoListItem.jsx
@@ -4,21 +4,21 @@ import { PencilIcon, TrashIcon } from "@heroicons/react/16/solid";
 
 
 const ToDoListItem = ({ title }) => {
-  return (
-    <div className="item mt-[15px] p-[10px]">
-      <div className="item-text">
-        <p>{title}</p>
-      </div>
-      <div className="item-actions">
-        <button className="btn-edit">
-          <PencilIcon className="w-5 h-5 text-blue-500" />
-        </button>
-        <button className="btn-delete">
-          <TrashIcon className="w-5 h-5 text-red-500" />
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-green-300 item radius-[5px] shadow-lg shadow-dark-800 mt-[15px] p-[10px] pl-[20px]">
+            <div className="item-text">
+                <p className="text-xl">{title}</p>
+            </div>
+            <div className="item-actions">
+                <button className="mr-[20px] outline outline-offset-4 outline-blue-500 rounded-[2px] cursor-pointer">
+                    <PencilIcon className="w-5 h-5 text-blue-500" />
+                </button>
+                <button className="outline outline-offset-4 outline-red-500 rounded-[2px] cursor-pointer">
+                    <TrashIcon className="w-5 h-5 text-red-500" />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default ToDoListItem;
